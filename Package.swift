ef4da97ae994cb359e6806b963fe3542f152a6d0
Pim <pim@pixelrock.nl>
2023-01-19 10:46:06 +0100

    Define supported platforms

--- Package.swift
+++ Package.swift
@@ -1,10 +1,17 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DidUpdate",
+	platforms: [
+		.iOS(.v11),
+		.macCatalyst(.v13),
+		.watchOS(.v5),
+		.tvOS(.v11),
+		.driverKit(.v19)
+	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
