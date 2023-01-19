46f7e1e5afb4ef5ff0174f81586c0cf13e3da92b
Pim <pim@pixelrock.nl>
2023-01-19 13:33:53 +0100

    Remove all unnecessary definitions

--- Package.swift
+++ Package.swift
@@ -1,33 +1,18 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DidUpdate",
-	platforms: [
-		.iOS(.v11),
-		.macCatalyst(.v13),
-		.watchOS(.v5),
-		.tvOS(.v11),
-		.driverKit(.v19)
-	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DidUpdate",
             targets: ["DidUpdate"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "DidUpdate",
-            dependencies: []),
+            name: "DidUpdate"),
         .testTarget(
             name: "DidUpdateTests",
             dependencies: ["DidUpdate"]),
